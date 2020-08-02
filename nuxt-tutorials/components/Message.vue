7fc08226ab129b051dcd274090f75f478096b125
Ngo Thanh Tung <tungnt@softech.vn>
2020-08-02 22:50:38 +0700

    Add NuxtJS

---
+++ nuxt-tutorials/components/Message.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>
+      {{ message }}
+    </h1>
+    <h1>
+      {{ ip }}
+    </h1>
+    <v-btn @click="$emit('click')">Onclick in Message Component</v-btn>
+    <v-btn @click="getIp">Get IP</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      default: '',
+    },
+  },
+
+  data: () => ({
+    ip: '',
+  }),
+
+  async mounted() {
+    const ip = await this.$axios.$get('http://icanhazip.com');
+    this.ip = ip;
+  },
+  methods: {
+    async getIp() {
+      const ip = await this.$axios.$get('http://icanhazip.com');
+      return { ip };
+    },
+  },
+};
+</script>
---
+++ nuxt-tutorials/pages/counter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <v-btn @click="increaseCount">Increase</v-btn>
+    <h1>{{ this.$store.state.counter }}</h1>
+    <p>{{ userAgent }}</p>
+    <Message @click="click('Hello')"></Message>
+  </div>
+</template>
+
+<script>
+import Message from '../components/Message';
+export default {
+  components: {
+    Message,
+  },
+
+  asyncData: ({ req }) => ({
+    userAgent: req
+      ? req.headers['user-agent']
+      : typeof navigator !== 'undefined'
+      ? navigator.userAgent
+      : 'No user agent (generated)',
+  }),
+
+  methods: {
+    increaseCount() {
+      this.$store.commit('increment');
+    },
+    click(name) {
+      // console.log('EMIT ON CLICK', name);
+    },
+  },
+};
+</script>
